7b3ab5b6945d6cde3a6bae65ae0aad1b396203ab
tadweereagle69-blip <tadweer.eagle69@gmail.com>
2026-08-12 13:38:33 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="adphc-field-inspection-localonly-v2";
+const CACHE="adphc-field-inspection-final-clean-v8";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
